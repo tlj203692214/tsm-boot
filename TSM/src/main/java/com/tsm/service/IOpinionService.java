c93d69a1e136b70fdd37d65051cdde490b17da92
203692214 <203692214@qq.com>
2021-12-24 19:37:34 +0800

    加opinion

---
+++ TSM/src/main/java/com/tsm/service/IOpinionService.java
@@ -0,0 +1,15 @@
+package com.tsm.service;
+
+import com.tsm.entity.Opinion;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 军
+ * @since 2021-12-22
+ */
+public interface IOpinionService extends IService<Opinion> {
+}
---
+++ TSM/src/main/java/com/tsm/service/impl/OpinionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tsm.service.impl;
+
+import com.tsm.entity.Opinion;
+import com.tsm.mapper.OpinionMapper;
+import com.tsm.service.IOpinionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 军
+ * @since 2021-12-22
+ */
+@Service
+public class OpinionServiceImpl extends ServiceImpl<OpinionMapper, Opinion> implements IOpinionService {
+
+}
